55abe5ed19eac432c9751ec233d78e69f1457397
u7145232 <u7145232@anu.edu.au>
2026-06-17 20:36:40 +1000

    add menu

--- src/scenes/character/character.gd
+++ src/scenes/character/character.gd
@@ -1,11 +1,11 @@
 class_name Character extends CharacterBody2D
 
-const MAX_SPEED: float = 150.0
-const ACCELERATION: float = 300.0
+const MAX_SPEED: float = 250.0
+#const ACCELERATION: float = 300.0
 
 var direction: Vector2 = Vector2.ZERO
-var current_velocity: Vector2 = Vector2.ZERO
-var target_velocity: Vector2 = Vector2.ZERO
+#var current_velocity: Vector2 = Vector2.ZERO
+#var target_velocity: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
 	pass
@@ -16,12 +16,12 @@ func _physics_process(delta: float) -> void:
 		int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
 	).normalized()
 	
-	if (direction.length() == 0):
-		target_velocity = Vector2.ZERO
-	else:
-		target_velocity = MAX_SPEED * direction
-		
-	current_velocity =current_velocity.move_toward(target_velocity, ACCELERATION * delta)
+	#if (direction.length() == 0):
+		#target_velocity = Vector2.ZERO
+	#else:
+		#target_velocity = MAX_SPEED * direction
+		#
+	#current_velocity =current_velocity.move_toward(target_velocity, ACCELERATION * delta)
 	
-	velocity = current_velocity
+	velocity = direction*MAX_SPEED
 	move_and_slide()
